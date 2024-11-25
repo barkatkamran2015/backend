39177f937fc98cc8a6dd83cff55bd7a5db9c062e
barkatkamran2015 <71576244+barkatkamran2015@users.noreply.github.com>
2024-11-24 16:41:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from app.blueprints.api import api_bp
+from yourapp.blueprints.api import api_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(api_bp, url_prefix='/api')
-    return app
 
-app = create_app()
+    @app.route('/', methods=['GET'])
+    def index():
+        return jsonify({'message': 'API is running'}), 200
+
+    return app
