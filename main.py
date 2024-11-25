44f2ac70477cd47ce99932f2c36392dc18d44920
barkatkamran2015 <71576244+barkatkamran2015@users.noreply.github.com>
2024-11-24 16:29:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# main.py
 from flask import Flask
 from flask_cors import CORS
 from app.blueprints.api import api_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # Enable CORS for all origins
+    CORS(app)
     app.register_blueprint(api_bp, url_prefix='/api')
     return app
 
+app = create_app()  # Create the app instance
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host='0.0.0.0', port=5000)
